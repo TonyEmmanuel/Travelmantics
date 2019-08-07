8c42e7a80eceeb9a10accee6a435dc6ef9cf64fe
tonyemmanuel <tonyemmanuel>
2019-08-07 10:11:04 -0700

    androidX refactor

--- app/src/main/java/com/example/travelmantics/DealActivity.java
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -3,8 +3,9 @@ package com.example.travelmantics;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
--- app/src/main/java/com/example/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/example/travelmantics/FirebaseUtil.java
@@ -1,7 +1,7 @@
 package com.example.travelmantics;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
